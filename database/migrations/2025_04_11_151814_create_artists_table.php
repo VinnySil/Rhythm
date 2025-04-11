6eff908298e693ae08f251e7ad9a0cd8576e9e20
Vinicius Silvestre Almeida <vinicius.silvestrefr@gmail.com>
2025-04-11 17:47:05 +0200

    email de artistas añadido

--- database/migrations/2025_04_11_151814_create_artists_table.php
+++ database/migrations/2025_04_11_151814_create_artists_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
             $table->string('name')->unique();
+            $table->string('email')->unique();
             $table->timestamps();
         });
     }
