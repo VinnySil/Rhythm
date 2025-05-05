328138876f77c1d97bc66020e0f060e4b58f4d27
Vinicius Silvestre Almeida <vinicius.silvestrefr@gmail.com>
2025-05-05 11:00:27 +0200

    migraciones, modelos y controladores de albums y songs

---
+++ database/migrations/2025_05_05_000006_create_songs_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('songs', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('artist_id')->constrained('artists')->onDelete('cascade');
+            $table->foreignId('album_id')->constrained('albums')->onDelete('cascade');
+            $table->string('tile');
+            $table->integer('duration'); //gaurdar la duracion en segundos
+            $table->string('song_cover');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('songs');
+    }
+};
